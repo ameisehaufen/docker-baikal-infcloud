59aa634c338e49b66cd769b73121d3d6be4cd903
Formiko <14934126+ameisehaufen@users.noreply.github.com>
2023-01-24 20:58:00 -0300

    Update infcloud.config.js

--- infcloud.config.js
+++ infcloud.config.js
@@ -752,7 +752,7 @@ var globalTimeZoneSupport=true;
 // timezones (defined in timezones.js):
 // grep "'[^']\+': {" timezones.js | sed -Ee "s#(\s*'|':\s*\{)##g"
 // Example:
-var globalTimeZone='Europe/Berlin';
+var globalTimeZone='America/Sao_Paulo';
 
 
 // globalTimeZonesEnabled
